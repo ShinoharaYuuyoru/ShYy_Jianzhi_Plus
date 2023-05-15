931cd5b50abb19081c91bb87cc17e1a69e7c98d7
ShinoharaYuuyoru <ShinoharaYuuyoru@gmail.com>
2023-05-16 01:20:01 +0900

    Update JZP1 & JZP2

--- Cpp_Solutions/JZP1.cpp
+++ Cpp_Solutions/JZP1.cpp
@@ -1,46 +1,47 @@
 class Solution {
    public:
-    int divide(int dividend, int divisor) {
+    int divide(int a, int b) {
         // 0x80000000: INT_MIN
         // 0x7fffffff: INT_MAX
         // 0xc0000000: INT_MIN / 2
 
+        int result = 0;
+
         // The case causes int type overflow in `result`
-        if ((dividend == (int)0x80000000) && (divisor == -1)) {
+        if ((a == (int)0x80000000) && (b == -1)) {
             return 0x7fffffff;
         }
         // The case causes int type overflow in `quotient` if continue calculating
-        if ((dividend == (int)0x80000000) && (divisor == 1)) {
-            return 0x80000000;
+        if ((a == (int)0x80000000) && (b == 1)) {
+            return (int)0x80000000;
         }
 
         // Confirm result positive or negative
-        // Change positive dividend/divisor to negative
+        // Change positive a/b to negative
         bool is_negative = false;
-        if (dividend > 0) {
-            dividend = 0 - dividend;
+        if (a > 0) {
+            a = 0 - a;
             is_negative = !is_negative;
         }
-        if (divisor > 0) {
-            divisor = 0 - divisor;
+        if (b > 0) {
+            b = 0 - b;
             is_negative = !is_negative;
         }
 
         // Do divide
-        int result = 0;
-        // dividend and divisor are all negative, so `<=`
-        while (dividend <= divisor) {
-            int now_divisor = divisor;
+        // a and b are all negative, so `<=`
+        while (a <= b) {
+            int now_divisor = b;
             int quotient = 1;
 
             // Get min now_divisor and max quotient
-            while ((now_divisor >= (int)0xc0000000) && (dividend <= (now_divisor + now_divisor))) {
+            while ((now_divisor >= (int)0xc0000000) && (a <= (now_divisor + now_divisor))) {
                 quotient += quotient;
                 now_divisor += now_divisor;
             }
 
             result += quotient;
-            dividend -= now_divisor;
+            a -= now_divisor;
         }
 
         result = (is_negative == false ? result : (0 - result));
--- Cpp_Solutions/JZP2.cpp
+++ Cpp_Solutions/JZP2.cpp
@@ -1,36 +1,31 @@
-#include <algorithm>
-#include <iostream>
-#include <string>
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string res = "";
 
-using namespace std;
+        unsigned int carry_bit = 0;
+        int i = a.size() - 1;  // Must use `int` type by using -1 as the end condition in following loop.
+        int j = b.size() - 1;
+        while (i >= 0 || j >= 0) {
+            unsigned int a_bit = (i >= 0 ? (a[i] - '0') : 0);
+            unsigned int b_bit = (j >= 0 ? (b[j] - '0') : 0);
 
-string addBinary(string a, string b) {
-    string res = "";
+            unsigned int sum = a_bit + b_bit + carry_bit;
+            unsigned int sum_bit = (sum >= 2 ? (sum - 2) : sum);
+            res.push_back(sum_bit + '0');
 
-    unsigned int carry_bit = 0;
-    int i = a.size() - 1;
-    int j = b.size() - 1;
-    while (i >= 0 || j >= 0) {
-        unsigned int a_bit = (i >= 0 ? a[i] - '0' : 0);
-        unsigned int b_bit = (j >= 0 ? b[j] - '0' : 0);
-        i--;
-        j--;
+            carry_bit = (sum >= 2 ? 1 : 0);
+            i--;
+            j--;
+        }
 
-        unsigned int sum = a_bit + b_bit + carry_bit;
-        carry_bit = (sum >= 2 ? 1 : 0);
-        unsigned int sum_bit = (sum >= 2 ? sum - 2 : sum);
+        // Set the highest bit with carry bit
+        if (carry_bit == 1) {
+            res.push_back('1');
+        }
 
-        char bit_res = '0' + sum_bit;
-        res.push_back(bit_res);
+        reverse(res.begin(), res.end());
+
+        return res;
     }
-    if (carry_bit == 1) {
-        res.push_back('1');
-    }
-
-    reverse(res.begin(), res.end());
-    return res;
-}
-
-int main() {
-    cout << addBinary("10101011101010101", "10000001111111") << endl;
-}
+};
